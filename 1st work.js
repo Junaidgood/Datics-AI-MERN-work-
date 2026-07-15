9ad98d08a0ace23c3752bc7af6395d384dd54461
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2026-07-15 18:46:58 +0500

    Add files via upload
    day 1 practice work for MERN in Datics Ai

---
+++ 1st work.js
@@ -0,0 +1,138 @@
+
+// console.log('very good');
+
+// data type practice
+
+// var a = "p";
+// var a = undefined;
+
+// Object practice
+
+// const n ="p";
+
+// const objt = {
+//     "mobile" : "infinix",
+//     "pen" : "fount",
+//     "king" : "ops",
+//     "reg" : undefined
+// }
+
+// objt["reg"] = "good"; // object re-assign
+// console.log(objt["mobile"]);
+
+
+// arithmetic oprations
+// let a=5;
+// let b=6;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a%b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(++a);
+// console.log(--b);
+// console.log(a++);
+// console.log(b++);
+// console.log(--b);
+// console.log(b--);
+// console.log(a--);
+// console.log(a++);
+
+
+
+// assignment oprations
+
+
+// let a=5;
+// a+=5;
+
+// console.log(a)
+// a-=5;
+
+// console.log(a)
+// a*=5;
+
+// console.log(a)
+// a/=5;
+
+
+// console.log(a)
+// a%=5;
+
+// console.log(a)
+// a=a+5;
+
+// console.log(a)
+
+// comperison oprators
+
+// let comp1 = 7;
+// let comp2 = 6;
+
+// console.log(comp1 ==comp2);
+// console.log(comp1===comp2);
+// console.log(comp1!=comp2);
+// console.log(comp1<comp2);
+// console.log(comp1>comp2);
+
+//logical oprators
+
+// let a=5;
+// let b=3;
+
+// console.log(a>b && b<a);
+// console.log(a<b || a<b)
+// console.log(a>b != b<a)
+
+// prompt and alert functions
+
+// let a =prompt("ur age?")
+
+// a = Number.parseInt(a);
+// if (a>0){
+// alert("valid")
+// }
+
+
+//ternary oprator:
+
+// let a=19 ;
+// let b=6;
+
+// console.log("you can", a<18 ? "not vote" : "vote")
+
+   //testing of typeof
+
+// let b = "7";
+// let a = "harry";
+
+// console.log(typeof(b))
+
+   // for in loop:
+   
+// let obj = {
+//     junaid:40,
+//     umer:30,
+//     riz:66
+    
+// }
+// for (let a in obj){
+// console.log(a,":",obj[a])
+// }
+
+
+  // for of loop:
+
+// for (let a of "junaid"){
+//  console.log(a)
+// }
+
+  //for loop
+
+for(var i=1;i<=4;i++){
+    console.log(i)
+}
+console.log("op")
+console.log(i)
+
