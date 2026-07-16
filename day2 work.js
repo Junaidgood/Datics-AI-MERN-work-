2cabfd26fb4855a32e734af62b361357134b9dbd
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2026-07-16 19:06:00 +0500

    Add files via upload
    Topic covers today
    
    Loops:
    While
    Do While
    
    Strings:
    
    JS strings methods
    Template literals
    Escape sequence
    
    Functions
    
    Arrays

---
+++ day2 work.js
@@ -0,0 +1,148 @@
+  //while loop.
+
+// const prompt = require('prompt-sync')();
+
+// let n = prompt("enter number ");
+// n = Number.parseInt(n);
+
+// let i=1;
+// while(i<=n){
+//     console.log(i);
+//     i++;
+// }
+
+
+//do-while loop
+
+// const prompt = require('prompt-sync')();
+
+// let n = prompt("enter number ");
+// n = Number.parseInt(n);
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=n);
+
+
+
+    //functions
+//     let a = 4;
+//     let b = 5;
+// function addin(x,y){
+//     return(x+y)/2
+// }
+
+
+
+// console.log(addin(a,b));
+
+    // arrow functions.
+
+//     let a = 4;
+//     let b = 5;
+
+// var addin = (x,y)=>{
+//     console.log("kamal")
+//     return(x+y)/2
+// }
+
+
+
+// console.log(addin(a,b));
+
+// let junaid={
+// "king":55,
+// "look" : 44,
+// "firs" : 2,
+// "oops" : 111
+// }
+
+// for(let key in junaid){
+//     console.log(key , junaid[key])
+// }
+
+
+        // problem
+
+// const prompt = require('prompt-sync')();
+// let cn = 4;
+// let i;
+// while(i!=cn){
+//     i = prompt ("Enter a number")
+// }
+
+
+   // length function use
+// let name = "junaid"
+// console.log(name.length)
+
+
+   //Template litrals
+
+// let name = "junaid";
+// let name1 = "umer";
+// let numb = 8
+
+// console.log(`1st name \t is ${name} and 2nd \t name \r is ${name1} ${numb}`)
+// console.log(`hel\^lo world \n this is hgngn\r junaid`)
+
+   //JS string methods
+
+// let name = "kingisking";
+// let player = "messi";
+// let player1 = "ronaldo"
+// let name1 = "      sda         "
+
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(player1.slice(2,-2))
+// console.log(name.replace("kin","jun"))
+// console.log(name.concat("is a ", player, "of football"))
+// console.log(name1.trim())
+// console.log(player.charAt(1))
+// console.log(player.charCodeAt(2))
+// console.log(player.repeat(5))
+// console.log(player.includes("i"))
+// console.log(player.startsWith("m"))
+// console.log(player.endsWith("s"))
+// console.log(player.split(""))
+// console.log(player.indexOf("i"))
+
+
+ //practice string methods 
+// let name = "king";
+
+// console.log(`the world's biggest "${name}" `)
+
+// let str = "give me 1000";
+
+// amount = Number.parseInt(str.slice("give me".length))
+// console.log(amount)
+
+
+// let name ="king";
+
+// console.log(name.replace("g","l"))
+
+
+
+    //Arrays
+
+// let marks = [44,55,11,22,33,66,77];
+
+// marks[2]=3
+
+// console.log(marks)
+
+    // iterate Array with for loop
+// let marks = [44,55,11,22,33,66,77];
+
+// marks[2]=3
+
+// for(let i = 0;i<marks.length;i++){
+//     console.log(marks[i])
+// }
