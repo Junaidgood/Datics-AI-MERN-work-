d6e34b77118fa91e74b6b6a6c330f506f9589cd4
M Junaid Siddiqi <mjunaidsiddiqi334@gmail.com>
2026-07-20 18:59:32 +0500

    Add files via upload
    event handlers ,asynchoronous, synchoronous

---
+++ day4-work.js
@@ -0,0 +1,62 @@
+   //setTimeout
+
+// document.write("hello")
+
+// let a = setTimeout(function(){
+//     alert("i am inside it")
+// },4000)
+
+// console.log(a)
+
+
+  
+   
+// let x = function(e){
+//     alert("hello there")
+// }
+
+// let y = function(e){
+//     alert("hel there")
+// }
+
+
+
+// btn.addEventListener('click',y)
+
+// let M = function(e){
+//     alert("hello there")
+// }
+
+// let N = function(e){
+//     alert("hel there")
+// }
+
+// btn.addEventListener('click',M)
+
+// btn.addEventListener('click',N)
+
+// let a = prompt("what is you favourite num")
+
+// if(a==3){
+//     btn.removeEventListener('click', M)
+//     }
+
+    
+
+
+// let x = function(el){
+//     console.log(el.target)
+//     console.log(el.type)
+//     alert("hello")
+    
+// }
+// console.log(typeof btn.addEventListener);
+// btn.addEventListener('click',x)
+
+
+   //asychoronous prog.
+
+// console.log("mobile")
+// setTimeout(function(){
+// console.log("junaid")
+// },3000)
